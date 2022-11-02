d8763ec5b5600357fd8b1129a3d8da30a28060e8
Asyraf <asyraf.wahianuar@gmail.com>
2022-11-02 14:17:42 +0800

    google font

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -25,6 +25,7 @@ $a_name = $this->request->getParam('action');
       href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
       rel="stylesheet"
     />
+    <link href="https://fonts.googleapis.com/css2?family=Squada+One&family=Tourney:wght@100&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Icons. Uncomment required icon fonts -->
 	<?php echo $this->Html->css('boxicons') ?>

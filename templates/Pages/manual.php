531b856b494f8bb366164a017ae3c8cbe394d2d5
Asyraf <asyraf.wahianuar@gmail.com>
2022-11-02 13:52:30 +0800

    docs

--- templates/Pages/manual.php
+++ templates/Pages/manual.php
@@ -1,15 +1,67 @@
-
+<h1 class="m-0 me-2 page_title">Re-CRUD Documentation</h1>
+<small class="text-muted"><?php echo $system_name; ?></small>
 
 
 
 <div class="row">
 	<div class="col-md-3">
- <a href="#i1">top</a>
-  <a href="#i2">top</a>
+		<div class="card shadow">
+			<div class="card-body">
+
+INITIAL CONFIGURATION
+<ul>
+<li><?php echo $this->Html->link('Basic Requirements','#a1',['class' => '']); ?></li>
+<li><?php echo $this->Html->link('Database Configuration','#a2',['class' => '']); ?></li>
+<li><?php echo $this->Html->link('Database Migration','#',['class' => '']); ?></li>
+<li><?php echo $this->Html->link('Database Data Seeding','#',['class' => '']); ?></li>
+<li><?php echo $this->Html->link('Email Configuration','#',['class' => '']); ?></li>
+<li><?php echo $this->Html->link('Default Table Migration','#',['class' => '']); ?></li>
+</ul>
+
+
+CRUD OPERATION
+<ul>
+<li><?php echo $this->Html->link('Command List','#',['class' => '']); ?></li>
+<li><?php echo $this->Html->link('Execute CRUD Operation Requirements','#',['class' => '']); ?></li>
+</ul>
+
+AUTHENTICATION &amp; AUTHORIZATION
+<ul>
+<li><?php echo $this->Html->link('Authentication','#',['class' => '']); ?></li>
+<li><?php echo $this->Html->link('Authorization','#',['class' => '']); ?></li>
+</ul>
+
+SEARCH
+<ul>
+<li><?php echo $this->Html->link('Basic Function','#',['class' => '']); ?></li>
+</ul>
+
+FORM FEATURES
+<ul>
+<li><?php echo $this->Html->link('Basic','#',['class' => '']); ?></li>
+<li><?php echo $this->Html->link('Basic','#',['class' => '']); ?></li>
+<li><?php echo $this->Html->link('Basic','#',['class' => '']); ?></li>
+<li><?php echo $this->Html->link('Basic','#',['class' => '']); ?></li>
+</ul>
+			</div>
+		</div>
+
 	</div>
 	<div class="col-md-9">
-<h2 id="i1">Lorem Ipsum</h2>
-<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-<h2 id="i2">Lorem Ipsum</h2>
+<div class="card shadow">
+	<div class="card-header d-flex align-items-center justify-content-between border-bottom">
+		<div class="card-title mb-0">
+		  <h1 class="m-0 me-2 page_title">Initial Configuration</h1>
+		  <small class="text-muted"><?php echo $system_name; ?></small>
+		</div>
+	</div>
+	<div class="card-body mt-4">
+<h5 id="a1">Basic Requirements</h5>
+<br/>
+<h5 id="a2">Database Configuration</h5>
+	</div>
+</div>	
+	
+
 	</div>
 </div>

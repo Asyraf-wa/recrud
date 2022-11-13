e4a477aa0d7e3e219c5ad540f3f24d8d215cc5ff
Asyraf <asyraf.wahianuar@gmail.com>
2022-11-13 18:28:27 +0800

    update prefix

--- templates/element/menu/top_bar.php
+++ templates/element/menu/top_bar.php
@@ -3,7 +3,7 @@
 		<div class="badge_card text-end mx-3">
 		<button class="badge_custom bg-light border-0"><i class="fa-regular fa-calendar"></i> <?php echo date("M d, Y"); ?></button>
 		<button class="badge_custom bg-light border-0" onclick="toggleFull()" alt=""><i class="fa-solid fa-expand"></i></button>
-		<?php echo $this->Html->link('<i class="fa-solid fa-arrow-right-from-bracket"></i>', ['controller' => 'Users', 'action' => 'logout'], ['class' => 'badge_custom_link bg-light border-0 kosong', 'escape' => false, 'alt' => 'Sign-out']); ?>
+		<?php echo $this->Html->link('<i class="fa-solid fa-arrow-right-from-bracket"></i>', ['controller' => 'Users', 'action' => 'logout', 'prefix' => false], ['class' => 'badge_custom_link bg-light border-0 kosong', 'escape' => false, 'alt' => 'Sign-out']); ?>
 		<script>
 		// full screen toggle
 		function toggleFull() {
